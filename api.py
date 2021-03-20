2a31c148af97255fab1782ed182ee4cb653f4da6
wahabmalik <wahabmalik125@gmail.com>
2021-03-20 18:18:40 +0500

    api uploaded

---
+++ api.py
@@ -0,0 +1,19 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return 'Hello'
+
+@app.route('/about')
+def about():
+    return 'this is company about page'
+
+@app.route('/about/<string:name>/<int:years>')
+def teamname(name ,years):
+    return 'Hello {name}, you have str(years) experience in NexAI'
+
+if __name__ == '__main__':
+    app.run(debug=True)
